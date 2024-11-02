35531835a68ba24627be38fd3c1993171ba7643a
Jackson Romie <jromie0924@gmail.com>
2024-11-02 12:02:31 -0500

    formatter for log file

--- src/app.py
+++ src/app.py
@@ -9,15 +9,16 @@ import sys
 def _init_logger():
   logger = logging.getLogger(config.APP_NAME)
   logger.setLevel(logging.INFO)
+  formatter = logging.Formatter('[%(asctime)s]:[%(levelname)s]:[%(module)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
   
   stream_handler = logging.StreamHandler(sys.stdout)
   
   if not config.IS_RASPBERRY_PI:
     file_handler = logging.FileHandler(config.LOG_FILE)
+    file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
   
   
-  formatter = logging.Formatter('[%(asctime)s]:[%(levelname)s]:[%(module)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
   stream_handler.setFormatter(formatter)
   logger.addHandler(stream_handler)
 
